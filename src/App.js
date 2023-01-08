9c6c989396001b999462a9802b07d44f00178729
Maksim <m.gavriuschin2016@yandex.ru>
2023-01-08 20:46:09 +0300

    dz-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import './App.css';
-import { Message } from "./components/Message";
+import { useState, useEffect } from 'react'
+import { Form } from './components/Form/Form'
+import { MessageList } from './components/MessageList/MessageList'
+import { AUTHOR } from './constants'
 
-function App() {
+export function App() {
+  const [messages, setMessages] = useState([])
+
+  const addMessage = (newMessage) => {
+    console.log('newMessage', newMessage);
+    setMessages([...messages, newMessage])
+  }
+
+  useEffect(() => {
+    if (messages.length > 0 && messages[messages.length - 1].author === AUTHOR.user) {
+      const timeout = setTimeout(() => {
+        addMessage({
+          author: AUTHOR.bot,
+          text: 'Im BOT'
+        })
+      }, 1500)
+
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  },)
+
   return (
     <>
-      <Message name="Maksim"></Message>
+      <h1>Welcome to chat!</h1>
+      <Form addMessage={addMessage} />
+      <MessageList messages={messages} />
     </>
-  );
+  )
 }
-
-export default App;
